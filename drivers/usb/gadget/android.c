e30b219763ec95245928ac9df9955aaf51d985ff
Songine <donglingluoying@gmail.com>
2022-09-22 20:03:18 +0800

    添加android-keyboard-gadget(USB-HID触摸板键盘)支持

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -38,6 +38,9 @@
 #include "f_mass_storage.c"
 #include "f_adb.c"
 #include "f_mtp.c"
+#include "f_hid.h"
+#include "f_hid_android_keyboard.c"
+#include "f_hid_android_mouse.c"
 #include "f_accessory.c"
 #define USB_ETH_RNDIS y
 #include "f_rndis.c"
@@ -1187,6 +1190,41 @@ static struct android_usb_function audio_source_function = {
 	.attributes	= audio_source_function_attributes,
 };
 
+static int hid_function_init(struct android_usb_function *f, struct usb_composite_dev *cdev)
+{
+	return ghid_setup(cdev->gadget, 2);
+}
+
+static void hid_function_cleanup(struct android_usb_function *f)
+{
+	ghid_cleanup();
+}
+
+static int hid_function_bind_config(struct android_usb_function *f, struct usb_configuration *c)
+{
+	int ret;
+	printk(KERN_INFO "hid keyboard\n");
+	ret = hidg_bind_config(c, &ghid_device_android_keyboard, 0);
+	if (ret) {
+		pr_info("%s: hid_function_bind_config keyboard failed: %d\n", __func__, ret);
+		return ret;
+	}
+	printk(KERN_INFO "hid mouse\n");
+	ret = hidg_bind_config(c, &ghid_device_android_mouse, 1);
+	if (ret) {
+		pr_info("%s: hid_function_bind_config mouse failed: %d\n", __func__, ret);
+		return ret;
+	}
+	return 0;
+}
+
+static struct android_usb_function hid_function = {
+	.name		= "hid",
+	.init		= hid_function_init,
+	.cleanup	= hid_function_cleanup,
+	.bind_config	= hid_function_bind_config,
+};
+
 static struct android_usb_function *supported_functions[] = {
 	&ffs_function,
 	&adb_function,
@@ -1202,6 +1240,7 @@ static struct android_usb_function *supported_functions[] = {
 	&dm_function,
 	&conn_gadget_function,
 #endif
+	&hid_function,
 	NULL
 };
 
@@ -1417,6 +1456,8 @@ functions_store(struct device *pdev, struct device_attribute *attr,
 
 #endif
 	}
+
+	android_enable_function(dev, conf, "hid");
 
 	mutex_unlock(&dev->mutex);
 
